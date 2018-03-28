ebfb480d6cee0f22d14aa9cd3f731df2bd4b5e3b
Arit Amana <>
2018-03-28 19:27:07 -0400

    Testing Gram Creation

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -7,6 +7,15 @@ class GramsController < ApplicationController
     @gram = Gram.new
   end
 
-  
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
   
 end
